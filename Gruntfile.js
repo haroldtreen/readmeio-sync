d0c1d45275228586c743f2eec816b26df4f15290
Harold Treen <harold@mobify.me>
2015-06-11 16:46:08 -0700

    Add specs for authenticator

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          quiet: false, // Optionally suppress output to standard out (defaults to false)
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+        },
+        src: ['test/**/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('test', 'mochaTest');
+
+};
