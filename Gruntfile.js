b20f43ba0ce480fa6d81f99ca845756e9d3995cb
Harold Treen <harold@mobify.me>
2015-06-15 16:22:22 -0700

    Add function to flag the process as being in test mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
         options: {
           reporter: 'spec',
           quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+          require: [function() { process.env.TEST_MODE = true; }]
         },
         src: ['test/**/*.js']
       }
