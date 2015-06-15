d650773f00ac3d92c2c2b9825f38b4b5c70ba9a1
Harold Treen <harold@mobify.me>
2015-06-15 16:27:24 -0700

    Use spaces for indentation

--- test/authenticatorSpec.js
+++ test/authenticatorSpec.js
@@ -1,21 +1,23 @@
+'use strict';
+
 var fs = require('fs');
 var assert = require('chai').assert;
 var auth = require('../lib/authenticator');
 
-describe('Authenticator', function(){
-	it('should return a cookie', function(done) {
-		auth.createSession(function(cookie) {
-			assert(cookie._jar.store.idx['dash.readme.io'], 'Cookie was not returned');
-			done();
-		});
-	});
+describe('Authenticator', function() {
+    it('should return a cookie', function(done) {
+        auth.createSession(function(cookie) {
+            assert(cookie._jar.store.idx['dash.readme.io'], 'Cookie was not returned');
+            done();
+        });
+    });
 
-	it('should load settings from a json', function(){
-		var authJson = fs.readFileSync('config/auth.json');
-		var authConfigs = JSON.parse(authJson);
+    it('should load settings from a json', function() {
+        var authJson = fs.readFileSync('config/auth.json');
+        var authConfigs = JSON.parse(authJson);
 
-		Object.keys(authConfigs).forEach(function(key){
-			assert.equal(auth.configs[key], authConfigs[key]);
-		});
-	});
+        Object.keys(authConfigs).forEach(function(key) {
+            assert.equal(auth.configs[key], authConfigs[key]);
+        });
+    });
 });
