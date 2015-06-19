8dc03307607c6316e6d6b3278cc8bee33cb8d3cf
Harold Treen <harold@mobify.me>
2015-06-18 17:01:11 -0700

    [RTM-225] Adds basic cli for performing uploads/inits

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 #! /usr/bin/env node
 
+'use strict';
 
+var js = require('jsonfile');
+
+var authenticator = require('./lib/authenticator');
+var Initializer = require('./lib/initializer');
+var Registry = require('./lib/registry');
+var Uploader = require('./lib/uploader');
+
+
+authenticator.createSession(function(cookie) {
+    if (process.argv[2] === 'upload') {
+        console.log('Upload!');
+        var registry = new Registry();
+        registry.import(js.readFileSync('syncRegistry.json'));
+
+        var uploader = new Uploader(registry);
+        uploader.uploadDocs(cookie, function(uploadedRegistry) {
+            uploadedRegistry.save('./');
+        });
+    } else if (process.argv[2] === 'init') {
+        console.log('Init!');
+        Initializer.initProjectInfo('./', cookie, function(initRepository) {
+            console.log('Done!');
+        });
+    }
+});
