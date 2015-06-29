4fcb89362d3ff481997e14427d295f2335860ee5
Harold Treen <harold@mobify.me>
2015-06-29 13:43:36 -0700

    Adds cleaner object for cleaning remote content.

---
+++ test/cleanerSpec.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var js = require('jsonfile');
+var assert = require('chai').assert;
+var nock = require('nock');
+
+var UrlGenerator = require('../lib/urlGenerator');
+var Registry = require('../lib/registry');
+var Cleaner = require('../lib/cleaner');
+
+var urlGen = new UrlGenerator('github-upload');
+
+var localRegistry = new Registry();
+var remoteRegistry = new Registry();
+
+var mockRemoteRegistryGet = function(scope) {
+    var urlGen1 = new UrlGenerator('github-upload', 'v1.0');
+    var urlGen2 = new UrlGenerator('github-upload', 'v2.0');
+
+    ['docs', 'content', 'pages'].forEach(function(section) {
+        scope.get(urlGen1[section + 'Path']()).reply(200, js.readFileSync('test/fixtures/' + section + '-v1.json'));
+        scope.get(urlGen2[section + 'Path']()).reply(200, js.readFileSync('test/fixtures/' + section + '-v2.json'));
+    });
+    return scope;
+};
+
+var mockDocDelete = function(diff) {
+    var scope = nock(urlGen.base());
+
+    scope = mockRemoteRegistryGet(scope);
+
+    diff.deleted.allDocs.forEach(function(doc) {
+        urlGen.version = doc.version;
+        scope.delete(urlGen.docsDeletePath(doc.slug)).reply(200, { success: true });
+    });
+
+    return scope;
+};
+
+var mockPageDelete = function(diff) {
+    var scope = nock(urlGen.base());
+
+    scope = mockRemoteRegistryGet(scope);
+
+    diff.deleted.allCustomPages.forEach(function(page) {
+        urlGen.version = page.version;
+        scope.delete(urlGen.pagesDeletePath(page.slug)).reply(200, { success: true });
+    });
+
+    return scope;
+};
+
+describe('Cleaning', function() {
+    var diff;
+
+    before(function() {
+        var localData = js.readFileSync('test/fixtures/localSyncRegistry.json');
+        var remoteData = js.readFileSync('test/fixtures/remoteSyncRegistry.json');
+
+        localRegistry.import(localData);
+        remoteRegistry.import(remoteData);
+
+        diff = localRegistry.diff(remoteRegistry);
+    });
+
+    it('deletes docs not specified in the sync registry', function(done) {
+        var scope = mockDocDelete(diff);
+
+        var cleaner = new Cleaner(localRegistry, 'cookie');
+
+        cleaner.cleanDocs(function(failedDeletes) {
+            assert.lengthOf(failedDeletes, 0);
+            assert.isTrue(scope.isDone());
+
+            done();
+        });
+    });
+
+    it('deletes pages not specified in the sync registry', function(done) {
+        var scope = mockPageDelete(diff);
+
+        var cleaner = new Cleaner(localRegistry, 'cookie');
+
+        cleaner.cleanPages(function(failedDeletes) {
+            assert.lengthOf(failedDeletes, 0);
+            assert.isTrue(scope.isDone());
+
+            done();
+        });
+    });
+});
