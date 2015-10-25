769c1b373620f8d264a75bfad3bdedd97e6a4dc3
Harold Treen <haroldtreen@gmail.com>
2015-10-25 02:29:39 -0700

    Add code coverage + coveralls!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,12 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
 module.exports = function(grunt) {
 
     // Add the grunt-mocha-test tasks.
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
     grunt.initConfig({
         // Configure a mochaTest task
@@ -15,9 +20,34 @@ module.exports = function(grunt) {
                 },
                 src: ['test/**/*.js']
             }
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: 'test/*.js', // a folder works nicely
+                options: {
+                    mask: '*Spec.js'
+                }
+            }
         }
     });
 
+    grunt.registerTask('setTestMode', function() {
+        process.env.TEST_MODE = 'true';
+    });
+
+    grunt.registerTask('sendToCoveralls', function() {
+        exec('cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js', function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Report Sent!');
+            }
+        });
+    });
+
+
+    grunt.registerTask('coverage', ['setTestMode', 'mocha_istanbul:coverage']);
+    grunt.registerTask('coveralls', 'coverage', 'sendToCoveralls');
     grunt.registerTask('test', 'mochaTest');
 
 };
